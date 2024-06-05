6f919bb05212cfe69659589a060094b0f71a9fe8
jasimahmdq2 <jasimahmed90852417@gmail.com>
2024-06-05 21:43:31 +0600

    m

--- src/components/HomeScreen/comment/commentCard.jsx
+++ src/components/HomeScreen/comment/commentCard.jsx
@@ -3,16 +3,16 @@ import MessageComponent from "./MessageComponent";
 const CommentCard = () => {
   return (
     <div>
-      {/* <MessageComponent />
+      <MessageComponent />
       <MessageComponent />
       <MessageComponent />
       <MessageComponent />
       {/* view reply */}
-      {/* <div className="text-center cursor-pointer mt-4">
+      <div className="text-center cursor-pointer mt-4">
         <p className="text-[#64748B] font-bold">View 30 Replies</p>
       </div>
 
-      <AddComment /> */} 
+      <AddComment />
     </div>
   );
 };
